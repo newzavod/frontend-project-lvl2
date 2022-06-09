0ae8fd9409156a504b563e01bc7c7fa79b92f36e
Vladimir <newzavod@gmail.com>
2022-06-09 08:56:17 +0300

    modified gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,11 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+    .name('gendiff.js')
     .description('CLI to some JavaScript string utilities')
-    .version('0.0.1', '-v, --vers', 'output the current version');
+    .version('0.0.1', '-v, --vers', 'output the current version')
+    .arguments('<filepath1>, <filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 program.command('split')
     .description('Split a string into substrings and display as an array')
