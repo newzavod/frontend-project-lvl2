94dd04f9821aad253b04711e99bd1c7162f0f8b9
Vladimir <newzavod@gmail.com>
2022-06-15 16:29:41 +0300

    add eslinttrc, index.js

--- src/index.js
+++ src/index.js
@@ -2,64 +2,84 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 
 const gendiff = (data1, data2) => {
-    const keys1 = Object.keys(data1);
-    const keys2 = Object.keys(data2);
-    const keys = _.union(keys1, keys2);
-    for (const key of keys) {
+    let result = {};
 
-        console.log(`${key} : ${data1[key]}`);
-        console.log(`${key} : ${data2[key]}`);
-    }
+    const keys1 = _.keys(data1);
+    const keys2 = _.keys(data2);
+    const keys = _.sortBy(_.union(keys1, keys2));
+    // console.log(keys); // [ 'host', 'timeout', 'proxy', 'follow', 'verbose' ]
+    // console.log(keys); // [ 'follow', 'host', 'proxy', 'timeout', 'verbose' ]
 
-    const result = {};
+    result = keys.map((key) => {
+        if (!_.has(data1, key)) {
+            return {
+                name: key,
+                value: data2[key],
+                type: 'added',
+            };
+        }
+        if (!_.has(data2, key)) {
+            return {
+                name: key,
+                value: data1[key],
+                type: 'deleted',
+            };
+        }
+        if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+            return {
+                name: key,
+                type: 'nested',
+                children: gendiff(data1[key], data2[key])
+            };
+        }
+        if (data1[key] !== data2[key]) {
+            return {
+                name: key,
+                value1: data1[key],
+                value2: data2[key],
+                type: 'changed',
+
+            };
+        }
+        return {
+            name: key,
+            value: data1[key],
+            type: 'unchanged',
+        };
+    });
+
     return result;
 };
 
 export default (filepath1, filepath2) => {
     const data1 = readFileSync(filepath1, 'utf-8');
     const data2 = readFileSync(filepath2, 'utf-8');
+    // console.log(data1);
+    // {
+    //     "host": "hexlet.io",
+    //     "timeout": 50,
+    //     "proxy": "123.234.53.22",
+    //     "follow": false
+    // }
+    // console.log(data2);
+    // {
+    //     "timeout": 20,
+    //     "verbose": true,
+    //     "host": "hexlet.io"
+    // }
+
     const obj1 = JSON.parse(data1);
     const obj2 = JSON.parse(data2);
 
-
-    gendiff(obj1, obj2);
-
-    // console.log(_.isEqual(obj1, obj2)); false
-    // console.log(obj1);
-    // console.log(obj2);
-
-    //obj1↓
-    //{
-    //   host: 'hexlet.io',
-    //   timeout: 50,
-    //   proxy: '123.234.53.22',
-    //   follow: false
-    // }
-    //obj2↓
-    //{ timeout: 20, verbose: true, host: 'hexlet.io' }
+    console.log(gendiff(obj1, obj2));
 
 };
-
-
-// export default genDiff;
-//!!!
-// const genDiff = (data1, data2) => {
-//     const keys1 = _.keys(data1);
-//     const keys2 = _.keys(data2);
-//     const keys = _.union(keys1, keys2);
-
-//     const result = {};
-//     for (const key of keys) {
-//         if (!_.has(data1, key)) {
-//             result[key] = 'added';
-//         } else if (!_.has(data2, key)) {
-//             result[key] = 'deleted';
-//         } else if (data[key] !== data2[key]) {
-//             result[key] = 'changed';
-//         } else {
-//             result[key] = 'unchanged';
-//         }
-//     }
-//     return result;
+//obj1↓
+//{
+//   host: 'hexlet.io',
+//   timeout: 50,
+//   proxy: '123.234.53.22',
+//   follow: false
 // }
-// export default genDiff;
+//obj2↓
+//{ timeout: 20, verbose: true, host: 'hexlet.io' }
